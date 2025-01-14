e272e5e65916c0140220bdcce15d13df611d9e88
gauravexampil <gaurav.exampil@gmail.com>
2025-01-15 03:32:19 +0530

    latest oush

--- forms/newsletter.php
+++ forms/newsletter.php
@@ -24,14 +24,14 @@
   $contact->subject ="New Subscription: " . $_POST['email'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
+    'host' => 'fixlii.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['email'], 'Email');
 
